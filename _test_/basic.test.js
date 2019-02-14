c3e4ecb6a520cba5e6f617779e5a9a8012af129e
Alistair Blake <consultations@me.com>
2019-02-14 14:50:07 -0800

    updated setup

--- _test_/basic.test.js
+++ _test_/basic.test.js
@@ -2,7 +2,19 @@
 
 const greet = require('./../greet');
 const faker = require('faker');
+const arithmetic = require('arithmetic');
 
+describe('#basic.test.js', () => {
+    //greet test
+}
+    test('should return Hello world', () => {
+        const returnWorld = greet.hello('world');
+        expect(returnWorld).toEqual('Hello world!');
+    });
+    test('should return null if not a string', () => {
+        const returnNull = greet.hello(faker.random.number());
+        expect(returnNull).toBeNull();
+    });
 // ARRANGE
 
 //ACT
--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const faker = require('faker');
 const greet = require('./greet');
 const arithmetic = require('arithmetic');
 
-
+console.log(greet.hello(faker.name.firstName()));
+console.log(`The sum of values in the array is: ${arithmetic.add(testArr)}.`);
+console.log(`The difference in values in the array is: ${arithmetic.subtract(testArr)}.`);
+console.log(`The product of the values in the array is: ${arithmetic.multiply(testArr)}.`);
+console.log(`The quotient of the values in the array is: ${arithmetic.divide(testArr)}.`);
